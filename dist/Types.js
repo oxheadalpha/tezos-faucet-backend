4deae8c8db472b545d15d0ef047d419adffe4834
Aryeh Harris <harryttd@gmail.com>
2023-07-28 17:19:40 -0400

    Don't track dist/

--- dist/Types.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Profile = void 0;
-var Profile;
-(function (Profile) {
-    Profile["USER"] = "USER";
-    Profile["BAKER"] = "BAKER";
-})(Profile || (Profile = {}));
-exports.Profile = Profile;
